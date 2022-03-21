5f287bcd46e749c3d10ad0c737fffeb543219fd9
BOBO1997git <yh198595@gmail.com>
2022-03-21 23:19:38 +0900

    added file

--- bell_ineq/libs_bell/lib_graph_states.py
+++ bell_ineq/libs_bell/lib_graph_states.py
@@ -1,3 +1,5 @@
+from mimetypes import init
+from pandas import RangeIndex
 from qiskit import QuantumCircuit
 
 def adjacency_list_for_path_graph(n):
@@ -15,6 +17,24 @@ def adjacency_list_for_path_graph(n):
         for j in range(1, n - 1):
             graph.append([j - 1, j + 1])
         graph.append([n - 2])
+    return graph
+
+
+def adjacency_list_for_cycle_graph(n):
+    """
+    Input
+        n : int, size of the path graph
+    Output
+        graph : list of list, adjacency list of the path graph
+    """
+    graph = []
+    if n == 1:
+        graph.append([])
+    else:
+        graph.append([n - 1, 1])
+        for j in range(1, n - 1):
+            graph.append([j - 1, j + 1])
+        graph.append([n - 2, 0])
     return graph
 
 
@@ -76,13 +96,38 @@ def path_graph_state(size, barrier=False):
         graph_state.barrier()
     for i in [i for i in range(size) if i % 2 == 0][:-1]:
         graph_state.cz(i, i + 1)
-    if size % 2 == 0:
+    if size % 2 == 0: # terminal 
         graph_state.cz(size - 2, size - 1)
     for i in [i for i in range(size) if i % 2 == 1][:-1]:
         graph_state.cz(i, i + 1)
-    if size % 2 == 1:
+    if size % 2 == 1: # terminal
         graph_state.cz(size - 2, size - 1)
     return graph_state
+
+
+def cycle_graph_state(size, barrier=False, initial_layout = None):
+    if size <= 1:
+        qc = QuantumCircuit(size)
+        return qc
+    if initial_layout is None:
+        initial_layout = list(range(size))
+    size = max(initial_layout) + 1
+    graph_state = QuantumCircuit(size)
+    graph_state.h(range(size))
+    if barrier:
+        graph_state.barrier()
+    for i in [i for i in initial_layout if i % 2 == 0][:-1]:
+        graph_state.cz(i, i + 1)
+    if size % 2 == 0: # terminal 
+        graph_state.cz(initial_layout[size - 2], initial_layout[size - 1])
+        graph_state.cz(initial_layout[size - 1], initial_layout[0])
+    for i in [i for i in initial_layout if i % 2 == 1][:-1]:
+        graph_state.cz(i, i + 1)
+    if size % 2 == 1: # terminal
+        graph_state.cz(initial_layout[size - 2], initial_layout[size - 1])
+        graph_state.cz(initial_layout[size - 1], initial_layout[0])
+    return graph_state
+
 
 def connection_graph_state(size, edges_order, barrier=False):
     if size <= 1:
